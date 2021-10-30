156b926f0ad95f0e21c6e6e26a2480a4e47481ab
Olesya <olesyapinky@gmail.com>
2021-10-30 22:02:41 +0300

    мпролд

---
+++ Numberr.js
@@ -0,0 +1,16 @@
+//Number
+const num = 42
+const float = 42.42
+const pow = 10e3
+console.log('MAX_SAFE_INTEGER', Number.MAX_SAFE_INTEGER);
+console.log('MIN_SAFE_INTEGER', Number.MIN_SAFE_INTEGER);
+console.log('Math.pow 53', Math.pow (2, 53)-1);
+console.log('MAX VALUE', Number.MAX_VALUE);
+console.log('MIN_VALUE', Number.MIN_VALUE);
+console.log('Positive infinity', Number.POSITIVE_INFINITY)
+console.log('Negative_infinity', Number.NEGATIVE_INFINITY)
+console.log(2/0)
+console.log(Number.NaN)
+console.log(typeof NaN)
+const weir = 2 / undefined
+console.log(Number.isNaN(weir));
